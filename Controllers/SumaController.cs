2efbd9eb5153964a6ef5619031b69293cb722dae
Josue Garay <Usuario@DESKTOP-EHLDOSJ>
2026-02-24 11:43:14 -0600

    Josue Avance

---
+++ Controllers/SumaController.cs
@@ -0,0 +1,20 @@
+﻿using J.M.K20260224.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace J.M.K20260224.Controllers
+{
+    public class SumaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(double numero1, double numero2)
+        {
+            double resultado = numero1 + numero2;
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+    }
+}
