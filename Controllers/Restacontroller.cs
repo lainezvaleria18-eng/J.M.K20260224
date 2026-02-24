0904a6db15048d4f6dec98cda134c1f5ea0ad2a1
MINEDUCYT <MINEDUCYT@YAMICOMPU>
2026-02-23 23:46:06 -0600

    Agregada vista de Resta
    Se agregó el controlador y la vista Index.cshtml

---
+++ Controllers/Restacontroller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace J.M.K20260224.Controllers
+{
+    public class RestaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(double numero1, double numero2)
+        {
+            double resultado = numero1 - numero2;
+            ViewBag.Resultado = resultado;
+            return View("Index");
+        }
+    }
+}
