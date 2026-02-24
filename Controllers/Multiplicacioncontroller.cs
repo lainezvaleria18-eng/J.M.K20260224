d7804f4c6ba4b1ecde6b862333573e09a85cd71b
valer <lainezvaleria18@gmail.com>
2026-02-24 09:42:00 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/Multiplicacioncontroller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using J.M.K20260224.Models;
+
+namespace J.M.K20260224.Controllers
+{
+    public class MultiplicacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calcular(Operacion op)
+        {
+            op.Resultado = op.Numero1 * op.Numero2;
+            return View("Index", op);
+        }
+    }
+}
---
+++ Models/Operacion.cs
@@ -0,0 +1,9 @@
+﻿namespace J.M.K20260224.Models
+{
+    public class Operacion
+    {
+        public double Numero1 { get; set; }
+        public double Numero2 { get; set; }
+        public double Resultado { get; set; }
+    }
+}
